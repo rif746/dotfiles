25d4e5b5895d679ea9442bcbeeb44f6e12b70bf3
Syarif Ubaidillah <ubed56pb@gmail.com>
2022-03-01 20:51:36 +0700

    perbaikan pada program praktikum di materi 6.

--- 6_Data Structure/praktikum/muncul_sekali/muncul_sekali.go
+++ 6_Data Structure/praktikum/muncul_sekali/muncul_sekali.go
@@ -2,53 +2,29 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func merge(num string) []int {
-	
-	number, _ := strconv.Atoi(num)
+func merge(arr string) []string {
+	keys := make(map[string]int)
+	list := []string{}
 
-	arr := splitDigit(number)
-
-	keys := make(map[int]bool)
-	del_list := []int{}
 	for _, entry := range arr {
-		if !keys[entry] {
-			keys[entry] = true
-		} else {
-			del_list = append(del_list, entry)
+		keys[string(entry)] += 1
+	}
+
+	for i, entry := range keys {
+		if entry == 1 {
+			list = append(list, i)
 		}
 	}
-	for _, v := range del_list {
-		arr = findAndDelete(arr, v)
-	}
-
-	return arr
-}
-
-func splitDigit(num int) []int {
-	var number []int
-	for num != 0 {
-		number = append(number, num % 10)
-		num /= 10
-	}
-	return number
-}
-
-func findAndDelete(arr []int, item int) []int {
-	var list []int
-	for _, v := range arr {
-		if v != item {
-			list = append(list, v)
-		}
-	}
+	
 	return list
 }
 
 func main() {
 	fmt.Println("Muncul Sekali")
 	arr := "12314527932"
+	merge(arr)
 	fmt.Println("Array : ", arr)
 	fmt.Println("Muncul Sekali : ", merge(arr))
 }
