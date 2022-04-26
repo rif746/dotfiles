37a26dea7a84d042059477ca7538b5956b06f9f6
Syarif Ubaidillah <ubed56pb@gmail.com>
2022-04-26 20:15:59 +0700

    menambahkan userscript bypass reddit untuk qutebrowser.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -72,6 +72,7 @@ def settings(c):
 def keybinding(c):
     c.bind('<Alt-y>', 'hint links userscript ytdl')
     c.bind('<Alt-v>', 'hint links userscript yt-mpv')
+    c.bind('<Alt-r>', 'hint links userscript reddit-bypass')
 
 
 keybinding(config)
